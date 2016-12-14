4e213fc77e79a35f86ca8efb138ddae3f9bc4c58
neuecc <ils@neue.cc>
2016-12-14 19:15:42 +0900

    Start codegenerator project

---
+++ src/MagicOnion.CodeGenerator/CodeAnalysis/Definitions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicOnion.CodeAnalysis
+{
+    public enum MethodType
+    {
+        Unary = 0,
+        ClientStreaming = 1,
+        ServerStreaming = 2,
+        DuplexStreaming = 3
+    }
+
+    public class InterfaceDefintion
+    {
+        public string Name { get; set; }
+        public string Namespace { get; set; }
+        public MethodDefinition[] Methods { get; set; }
+    }
+
+    public class MethodDefinition
+    {
+        public MethodType MethodType { get; set; }
+        public string RequestType { get; set; }
+        public string ResponseType { get; set; }
+        public ParameterDefinition[] Parameters { get; set; }
+    }
+
+    public class ParameterDefinition
+    {
+        public string TypeName { get; set; }
+        public string ParameterName { get; set; }
+    }
+}
---
+++ src/MagicOnion.CodeGenerator/Program.cs
@@ -0,0 +1,96 @@
+﻿using MagicOnion.CodeAnalysis;
+using Mono.Options;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicOnion.CodeGenerator
+{
+    class CommandlineArguments
+    {
+        // moc.exe
+
+        public string InputPath { get; private set; }
+        public string OutputPath { get; private set; }
+        public bool UnuseUnityAttr { get; private set; }
+        public List<string> ConditionalSymbols { get; private set; }
+        public bool IsSeparate { get; private set; }
+        public string NamespaceRoot { get; private set; }
+
+        public bool IsParsed { get; set; }
+
+        public CommandlineArguments(string[] args)
+        {
+            ConditionalSymbols = new List<string>();
+            NamespaceRoot = "MagicOnion";
+
+            var option = new OptionSet()
+            {
+                { "i|input=", "[required]Input path of analyze csproj", x => { InputPath = x; } },
+                { "o|output=", "[required]Output path(file) or directory base(in separated mode)", x => { OutputPath = x; } },
+                { "s|separate", "[optional, default=false]Output files are separated", _ => { IsSeparate = true; } },
+                { "u|unuseunityattr", "[optional, default=false]Unuse UnityEngine's RuntimeInitializeOnLoadMethodAttribute on ZeroFormatterInitializer", _ => { UnuseUnityAttr = true; } },
+                { "c|conditionalsymbol=", "[optional, default=empty]conditional compiler symbol", x => { ConditionalSymbols.AddRange(x.Split(',')); } },
+                { "n|namespace=", "[optional, default=ZeroFormatter]Set namespace root name", x => { NamespaceRoot = x; } },
+            };
+            if (args.Length == 0)
+            {
+                goto SHOW_HELP;
+            }
+            else
+            {
+                option.Parse(args);
+
+                if (InputPath == null || OutputPath == null)
+                {
+                    Console.WriteLine("Invalid Argument:" + string.Join(" ", args));
+                    Console.WriteLine();
+                    goto SHOW_HELP;
+                }
+
+                IsParsed = true;
+                return;
+            }
+
+            SHOW_HELP:
+            Console.WriteLine("moc arguments help:");
+            option.WriteOptionDescriptions(Console.Out);
+            IsParsed = false;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmdArgs = new CommandlineArguments(args);
+            if (!cmdArgs.IsParsed)
+            {
+                return;
+            }
+
+            // Generator Start...
+
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine("Project Compilation Start:" + cmdArgs.InputPath);
+
+            var collector = new MethodCollector(cmdArgs.InputPath, cmdArgs.ConditionalSymbols);
+
+            Console.WriteLine("Project Compilation Complete:" + sw.Elapsed.ToString());
+            Console.WriteLine();
+
+            sw.Restart();
+            Console.WriteLine("Method Collect Start");
+
+            var definitions = collector.Visit();
+
+            Console.WriteLine("Method Collect Complete:" + sw.Elapsed.ToString());
+
+            // TODO:Generate!
+        }
+    }
+}
