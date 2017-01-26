a1a153f4cc88ee90958da7d50c756f8c5aae3506
neuecc <ils@neue.cc>
2017-01-26 12:25:35 +0900

    fixed serverfixture

--- tests/MagicOnion.Tests/_ServerFixture.cs
+++ tests/MagicOnion.Tests/_ServerFixture.cs
@@ -1,10 +1,36 @@
 ﻿using Grpc.Core;
 using MagicOnion.Server;
 using System;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace MagicOnion.Tests
 {
+    public static class RandomProvider
+    {
+        [ThreadStatic]
+        static Random random;
+
+        public static Random ThreadRandom
+        {
+            get
+            {
+                if (random == null)
+                {
+                    using (var rng = new RNGCryptoServiceProvider())
+                    {
+                        var buffer = new byte[sizeof(int)];
+                        rng.GetBytes(buffer);
+                        var seed = BitConverter.ToInt32(buffer, 0);
+                        random = new Random(seed);
+                    }
+                }
+
+                return random;
+            }
+        }
+    }
+
     public class ServerFixture : IDisposable
     {
         Grpc.Core.Server server;
@@ -14,7 +40,7 @@ namespace MagicOnion.Tests
         {
             var service = MagicOnionEngine.BuildServerServiceDefinition(isReturnExceptionStackTraceInErrorDetail: true);
 
-            var port = new Random().Next(10000, 30000);
+            var port = RandomProvider.ThreadRandom.Next(10000, 30000);
             var serverPort = new ServerPort("localhost", port, ServerCredentials.Insecure);
 
             server = new global::Grpc.Core.Server
