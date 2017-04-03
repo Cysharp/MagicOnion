440bdbfa05aee11225b4f5544829b74cb3e2d57c
neuecc <ils@neue.cc>
2017-04-03 19:22:15 +0900

    prepare 0.4.0

---
+++ nuget/copy_moc.bat
@@ -0,0 +1,3 @@
+copy ..\src\MagicOnion.CodeGenerator\bin\Release\*.dll tools\
+copy ..\src\MagicOnion.CodeGenerator\bin\Release\mpc.exe tools\moc.exe
+copy ..\src\MagicOnion.CodeGenerator\bin\Release\mpc.exe.config tools\moc.exe.config
