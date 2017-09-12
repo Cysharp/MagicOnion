b39b523d658d7a6be73cce38c00a2da34d351a41
kado-yasuyuki <kado.yasuyuki@meleap.com>
2017-09-12 15:06:28 +0900

    stopping automatic compilation of scripts during play mode.

---
+++ src/MagicOnion.Client.Unity/Assets/Scripts/MagicOnion/Unity/CompileStopperDuringPlayMode.cs
@@ -0,0 +1,42 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+namespace MagicOnion
+{
+    [InitializeOnLoad]
+    public class CompileStopperDuringPlayMode
+    {
+        static bool isStopped = false;
+
+        static CompileStopperDuringPlayMode()
+        {
+            EditorApplication.update += OnEditorUpdate;
+        }
+
+        static void OnEditorUpdate()
+        {
+            if (!isStopped
+                && EditorApplication.isCompiling
+                && EditorApplication.isPlaying)
+            {
+                EditorApplication.LockReloadAssemblies();
+                EditorApplication.playmodeStateChanged += PlayModeChanged;
+                isStopped = true;
+                Debug.Log("Stop Compiling the scripts");
+            }
+        }
+
+        static void PlayModeChanged()
+        {
+            if (EditorApplication.isPlaying)
+                return;
+
+            EditorApplication.UnlockReloadAssemblies();
+            EditorApplication.playmodeStateChanged -= PlayModeChanged;
+            isStopped = false;
+            Debug.Log("Start Compiling the scripts");
+        }
+    }
+}
+#endif
