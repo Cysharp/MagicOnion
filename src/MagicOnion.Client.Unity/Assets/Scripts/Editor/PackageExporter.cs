0d149eb8b563bbdbeb3348a8ca2e826c49961283
neuecc <ils@neue.cc>
2019-05-13 17:21:24 +0900

    namespace is bad...

--- src/MagicOnion.Client.Unity/Assets/Scripts/Editor/PackageExporter.cs
+++ src/MagicOnion.Client.Unity/Assets/Scripts/Editor/PackageExporter.cs
@@ -1,37 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 
-namespace UnityEditorUtility
+public static class PackageExporter
 {
-    public static class PackageExporter
+    [MenuItem("Tools/Export Unitypackage")]
+    public static void Export()
     {
-        [MenuItem("Tools/Export Unitypackage")]
-        public static void Export()
-        {
-            // configure
-            var root = "Scripts/MagicOnion";
-            var exportPath = "./MagicOnion.Client.Unity.unitypackage";
+        // configure
+        var root = "Scripts/MagicOnion";
+        var exportPath = "./MagicOnion.Client.Unity.unitypackage";
 
-            var path = Path.Combine(Application.dataPath, root);
-            var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
-                .Where(x => Path.GetExtension(x) == ".cs" || Path.GetExtension(x) == ".asmdef" || Path.GetExtension(x) == ".json")
-                .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
-                .ToArray();
+        var path = Path.Combine(Application.dataPath, root);
+        var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
+            .Where(x => Path.GetExtension(x) == ".cs" || Path.GetExtension(x) == ".asmdef" || Path.GetExtension(x) == ".json")
+            .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
+            .ToArray();
 
-            UnityEngine.Debug.Log("Export below files" + Environment.NewLine + string.Join(Environment.NewLine, assets));
+        UnityEngine.Debug.Log("Export below files" + Environment.NewLine + string.Join(Environment.NewLine, assets));
 
-            AssetDatabase.ExportPackage(
-                assets,
-                exportPath,
-                ExportPackageOptions.Default);
+        AssetDatabase.ExportPackage(
+            assets,
+            exportPath,
+            ExportPackageOptions.Default);
 
-            UnityEngine.Debug.Log("Export complete: " + Path.GetFullPath(exportPath));
-        }
+        UnityEngine.Debug.Log("Export complete: " + Path.GetFullPath(exportPath));
     }
 }
