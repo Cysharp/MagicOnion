4014d1b1c01058b28dc54241ecd6acfb27ed3f50
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-01-07 12:59:02 +0900

    fix: Update ChatApp.Server

--- samples/ChatApp/ChatApp.Server/Program.cs
+++ samples/ChatApp/ChatApp.Server/Program.cs
@@ -13,19 +13,10 @@ namespace ChatApp.Server
     {
         static async Task Main(string[] args)
         {
-            GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
-
-            // for SSL/TLS connection
-            //var config = new ConfigurationBuilder().AddEnvironmentVariables().Build();
-            //var certificates = new List<KeyCertificatePair> { new KeyCertificatePair(File.ReadAllText("server.crt"), File.ReadAllText("server.key")) };
-            //var credential = new SslServerCredentials(certificates);
+            //GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
 
             await MagicOnionHost.CreateDefaultBuilder()
-                .UseMagicOnion(
-                    new MagicOnionOptions(isReturnExceptionStackTraceInErrorDetail: true),
-                    new ServerPort("localhost", 12345, ServerCredentials.Insecure))
-                    // for SSL/TLS Connection
-                    //new ServerPort(config.GetValue<string>("MAGICONION_HOST", "127.0.0.1"), 12345, credential))
+                .UseMagicOnion()
                 .RunConsoleAsync();
         }
     }
