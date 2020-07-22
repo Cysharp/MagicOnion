83c9ec12c0bae647cd23d2358a098fc46b9b3fe2
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-07-22 16:38:19 +0900

    chore: Add an ASP.NET Core-based sandbox project

---
+++ sandbox/Sandbox.AspNetCore/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MagicOnion;
+using MagicOnion.Server;
+
+namespace Sandbox.AspNetCore.Services
+{
+    public interface IGreeterService : IService<IGreeterService>
+    {
+        UnaryResult<string> HelloAsync();
+    }
+
+    public class GreeterService : ServiceBase<IGreeterService>, IGreeterService
+    {
+        public UnaryResult<string> HelloAsync()
+        {
+            return UnaryResult("Konnichiwa from MagicOnion + ASP.NET Core");
+        }
+    }
+}
