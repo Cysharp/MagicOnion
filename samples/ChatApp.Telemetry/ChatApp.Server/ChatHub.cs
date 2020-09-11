8d1136fc16622f1903a4cb748db195cb0ba50491
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-09-11 16:06:50 +0900

    Fix ChatApp.Telemetry build error

--- samples/ChatApp.Telemetry/ChatApp.Server/ChatHub.cs
+++ samples/ChatApp.Telemetry/ChatApp.Server/ChatHub.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using MagicOnion.Server.OpenTelemetry;
 
 namespace ChatApp.Server
 {
--- samples/ChatApp.Telemetry/ChatApp.Server/Program.cs
+++ samples/ChatApp.Telemetry/ChatApp.Server/Program.cs
@@ -1,58 +1,20 @@
-using Grpc.Core;
-using MagicOnion.Hosting;
-using MagicOnion.OpenTelemetry;
-using MagicOnion.Server;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenTelemetry.Exporter.Prometheus;
-using OpenTelemetry.Exporter.Zipkin;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Trace;
-using System;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ChatApp.Server
 {
     class Program
     {
-        static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            //GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
-            
-            await MagicOnionHost.CreateDefaultBuilder()
-                .UseMagicOnion()
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddMagicOnionOpenTelemetry((options, meterOptions) =>
-                    {
-                        // open-telemetry with Prometheus exporter
-                        meterOptions.MetricExporter = new PrometheusExporter(new PrometheusExporterOptions() { Url = options.MetricsExporterEndpoint });
-                    },
-                    (options, provider, tracerBuilder) =>
-                    {
-                        // open-telemetry with Zipkin exporter
-                        tracerBuilder.AddZipkinExporter(o =>
-                        {
-                            o.ServiceName = "ChatApp.Server";
-                            o.Endpoint = new Uri(options.TracerExporterEndpoint);
-                        });
-                        // ConsoleExporter will show current tracer activity
-                        tracerBuilder.AddConsoleExporter();
-                    });
-                    services.AddHostedService<PrometheusExporterMetricsService>();
-                })
-                .ConfigureServices((hostContext, services) =>
-                {
-                    var meterProvider = services.BuildServiceProvider().GetService<MeterProvider>();
-                    services.Configure<MagicOnionHostingOptions>(options =>
-                    {
-                        options.Service.GlobalFilters.Add(new OpenTelemetryCollectorFilterFactoryAttribute());
-                        options.Service.GlobalStreamingHubFilters.Add(new OpenTelemetryHubCollectorFilterFactoryAttribute());
-                        options.Service.MagicOnionLogger = new OpenTelemetryCollectorLogger(meterProvider, version: "0.5.0-beta.2");
-                    });
-                })
-                .RunConsoleAsync();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
