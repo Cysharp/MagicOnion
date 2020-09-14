a3c715ab4385b56de4d3c6f694e8e797902f1d8b
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-09-14 15:48:21 +0900

    Remove Hosting and legacy-Server/Client

--- src/MagicOnion/Server/IMagicOnionFilterFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MagicOnion.Server
-{
-    public interface IMagicOnionFilterFactory<T>
-    {
-        T CreateInstance(IServiceLocator serviceLocator);
-        int Order { get; }
-    }
-}
