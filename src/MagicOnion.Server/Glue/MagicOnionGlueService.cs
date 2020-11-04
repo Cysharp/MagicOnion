b00136447aaba7cb32cd13546c0b23d178e96ae5
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-11-04 12:09:05 +0900

    Add no-op BindMethod

--- src/MagicOnion.Server/Glue/MagicOnionGlueService.cs
+++ src/MagicOnion.Server/Glue/MagicOnionGlueService.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
+using Grpc.Core;
 
 namespace MagicOnion.Server.Glue
 {
-    internal static class MagicOnionGlueService
+    internal class MagicOnionGlueService
     {
         public static Type CreateType()
         {
@@ -16,9 +17,16 @@ namespace MagicOnion.Server.Glue
             var type = typeBuilder.CreateType()!;
             return typeof(MagicOnionGlueService<>).MakeGenericType(type);
         }
+
+        public static void BindMethod(ServiceBinderBase binder, MagicOnionGlueService service)
+        {
+            // no-op at here.
+            // The MagicOnion service methods are bound by `MagicOnionGlueServiceMethodProvider<TService>`
+        }
     }
 
-    internal class MagicOnionGlueService<TService>
+    [BindServiceMethod(typeof(MagicOnionGlueService), nameof(BindMethod))]
+    internal class MagicOnionGlueService<TService> : MagicOnionGlueService
         where TService : class
     {
     }
