d05d1bd1b1b3186b66a40329cc77fd87b83f21aa
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-11-11 17:00:29 +0900

    Remove enforcing HTTPS

--- samples/JwtAuthentication/JwtAuthApp.Server/Startup.cs
+++ samples/JwtAuthentication/JwtAuthApp.Server/Startup.cs
@@ -45,13 +45,6 @@ namespace JwtAuthApp.Server
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
