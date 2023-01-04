fd5c14fb2fad8c5596a21836c00b33b6bde7a7e3
Mayuki Sawatari <mayuki+github@misuzilla.org>
2023-01-04 15:57:12 +0900

    Rewriting to Top-level statements style.

--- samples/ChatApp/ChatApp.Server/Program.cs
+++ samples/ChatApp/ChatApp.Server/Program.cs
@@ -1,35 +1,21 @@
-using System;
-using Grpc.Core;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace ChatApp.Server
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(options =>
 {
-    class Program
+    // WORKAROUND: Accept HTTP/2 only to allow insecure HTTP/2 connections during development.
+    options.ConfigureEndpointDefaults(endpointOptions =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        endpointOptions.Protocols = HttpProtocols.Http2;
+    });
+});
+builder.Services.AddGrpc();  // MagicOnion depends on ASP.NET Core gRPC service.
+builder.Services.AddMagicOnion();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder
-                        .UseKestrel(options =>
-                        {
-                            // WORKAROUND: Accept HTTP/2 only to allow insecure HTTP/2 connections during development.
-                            options.ConfigureEndpointDefaults(endpointOptions =>
-                            {
-                                endpointOptions.Protocols = HttpProtocols.Http2;
-                            });
-                        })
-                        .UseStartup<Startup>();
-                });
-    }
-}
+var app = builder.Build();
+app.MapMagicOnionService();
+
+app.Run();
