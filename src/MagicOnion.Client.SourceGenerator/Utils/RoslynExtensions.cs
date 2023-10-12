2fc3833c9f18678cca88b9c482adc819f70b7767
Mayuki Sawatari <mayuki+github@misuzilla.org>
2023-10-12 11:41:24 +0900

    Remove unused code

--- src/MagicOnion.Client.SourceGenerator/Utils/RoslynExtensions.cs
+++ src/MagicOnion.Client.SourceGenerator/Utils/RoslynExtensions.cs
@@ -1,126 +1,14 @@
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace MagicOnion.Client.SourceGenerator.Utils;
 
 // Utility and Extension methods for Roslyn
 internal static class RoslynExtensions
 {
-    public static IEnumerable<INamedTypeSymbol> GetNamedTypeSymbols(this Compilation compilation)
-    {
-        foreach (var syntaxTree in compilation.SyntaxTrees)
-        {
-            var semModel = compilation.GetSemanticModel(syntaxTree);
-
-            foreach (var item in syntaxTree.GetRoot()
-                         .DescendantNodes()
-                         .Select(x => semModel.GetDeclaredSymbol(x))
-                         .Where(x => x != null))
-            {
-                var namedType = item as INamedTypeSymbol;
-                if (namedType != null)
-                {
-                    yield return namedType;
-                }
-            }
-        }
-    }
-
-    public static IEnumerable<INamedTypeSymbol> EnumerateBaseType(this ITypeSymbol symbol)
-    {
-        var t = symbol.BaseType;
-        while (t != null)
-        {
-            yield return t;
-            t = t.BaseType;
-        }
-    }
-
-    public static AttributeData? FindAttribute(this IEnumerable<AttributeData> attributeDataList, string typeName)
-    {
-        return attributeDataList.FirstOrDefault(x => x.AttributeClass?.ToDisplayString() == typeName);
-    }
-
     public static AttributeData? FindAttributeShortName(this IEnumerable<AttributeData> attributeDataList,
         string typeName)
     {
         return attributeDataList.FirstOrDefault(x => x.AttributeClass?.Name == typeName);
-    }
-
-    public static AttributeData? FindAttributeIncludeBasePropertyShortName(this IPropertySymbol property, string typeName)
-    {
-        while (true)
-        {
-            var data = FindAttributeShortName(property.GetAttributes(), typeName);
-            if (data != null) return data;
-            if (property.OverriddenProperty is null)
-            {
-                break;
-            }
-            property = property.OverriddenProperty;
-        }
-
-        return null;
-    }
-
-    public static AttributeSyntax? FindAttribute(this BaseTypeDeclarationSyntax typeDeclaration, SemanticModel model, string typeName)
-    {
-        return typeDeclaration.AttributeLists
-            .SelectMany(x => x.Attributes)
-            .Where(x => model.GetTypeInfo(x).Type?.ToDisplayString() == typeName)
-            .FirstOrDefault();
-    }
-
-    public static INamedTypeSymbol? FindBaseTargetType(this ITypeSymbol symbol, string typeName)
-    {
-        return symbol.EnumerateBaseType()
-            .Where(x => x.OriginalDefinition?.ToDisplayString() == typeName)
-            .FirstOrDefault();
-    }
-
-    public static object? GetSingleNamedArgumentValue(this AttributeData attribute, string key)
-    {
-        foreach (var item in attribute.NamedArguments)
-        {
-            if (item.Key == key)
-            {
-                return item.Value.Value;
-            }
-        }
-
-        return null;
-    }
-
-    public static bool IsNullable(this INamedTypeSymbol symbol)
-    {
-        if (symbol.IsGenericType)
-        {
-            if (symbol.ConstructUnboundGenericType().ToDisplayString() == "T?")
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static IEnumerable<ISymbol> GetAllMembers(this ITypeSymbol symbol)
-    {
-        var t = symbol;
-        while (t != null)
-        {
-            foreach (var item in t.GetMembers())
-            {
-                yield return item;
-            }
-            t = t.BaseType;
-        }
-    }
-
-    public static IEnumerable<ISymbol> GetAllInterfaceMembers(this ITypeSymbol symbol)
-    {
-        return symbol.GetMembers()
-            .Concat(symbol.AllInterfaces.SelectMany(x => x.GetMembers()));
     }
 
     public static bool ApproximatelyEqual(this ITypeSymbol left, ITypeSymbol right)
