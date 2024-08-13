b5dffafe87b39334f03b75083f5fc93afd5d396a
Mayuki Sawatari <mayuki+github@misuzilla.org>
2024-08-13 16:16:51 +0900

    Revert: protected -> private

--- src/MagicOnion.Client.Unity/Assets/Scripts/MagicOnion/MagicOnion.Client/StreamingHubClientBase.cs
+++ src/MagicOnion.Client.Unity/Assets/Scripts/MagicOnion/MagicOnion.Client/StreamingHubClientBase.cs
@@ -186,7 +186,7 @@ namespace MagicOnion.Client
         StreamingHubClientHeartbeatManager heartbeatManager = default!;
         Task subscription = default!;
 
-        readonly TReceiver receiver;
+        protected readonly TReceiver receiver;
 
         protected StreamingHubClientBase(string serviceName, TReceiver receiver, CallInvoker callInvoker, StreamingHubClientOptions options)
         {
