7a5c4039c7ed58462253e0222b0c24d47e71d60d
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-09-11 19:18:33 +0900

    chore: add benchmark pattern

--- perf/BenchmarkApp/PerformanceTest.Shared/IPerfTestHub.cs
+++ perf/BenchmarkApp/PerformanceTest.Shared/IPerfTestHub.cs
@@ -1,22 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MagicOnion;
 
-namespace PerformanceTest.Shared
-{
-    public interface IPerfTestHub : IStreamingHub<IPerfTestHub, IPerfTestHubReceiver>
-    {
-        Task<int> CallMethodAsync(string arg1, int arg2, int arg3, int arg4);
-        ValueTask<int> CallMethodValueTaskAsync(string arg1, int arg2, int arg3, int arg4);
-        Task<ComplexResponse> CallMethodComplexAsync(string arg1, int arg2, int arg3, int arg4);
-        ValueTask<ComplexResponse> CallMethodComplexValueTaskAsync(string arg1, int arg2, int arg3, int arg4);
-        Task<(int StatusCode, byte[] Data)> CallMethodLargePayloadAsync(string arg1, int arg2, int arg3, int arg4, byte[] arg5);
-        Task<SimpleResponse> PingpongAsync(SimpleRequest request);
-    }
+namespace PerformanceTest.Shared;
 
-    public interface IPerfTestHubReceiver
-    {}
+public interface IPerfTestHub : IStreamingHub<IPerfTestHub, IPerfTestHubReceiver>
+{
+    Task<int> CallMethodAsync(string arg1, int arg2, int arg3, int arg4);
+    ValueTask<int> CallMethodValueTaskAsync(string arg1, int arg2, int arg3, int arg4);
+    Task<ComplexResponse> CallMethodComplexAsync(string arg1, int arg2, int arg3, int arg4);
+    ValueTask<ComplexResponse> CallMethodComplexValueTaskAsync(string arg1, int arg2, int arg3, int arg4);
+    Task<(int StatusCode, byte[] Data)> CallMethodLargePayloadAsync(string arg1, int arg2, int arg3, int arg4, byte[] arg5);
+    Task<SimpleResponse> PingpongAsync(SimpleRequest request);
 }
+
+public interface IPerfTestHubReceiver
+{}
