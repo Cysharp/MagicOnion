9896553af409ae666eb78428a01e4c98bdfe1928
Mayuki Sawatari <mayuki+github@misuzilla.org>
2024-12-27 18:23:17 +0900

    No use SteramingHubPayloadPool on server-side

--- src/MagicOnion.Internal/StreamingHubPayloadPool.ObjectPool.cs
+++ src/MagicOnion.Internal/StreamingHubPayloadPool.ObjectPool.cs
@@ -1,76 +1,53 @@
 #if USE_OBJECTPOOL_STREAMINGHUBPAYLOADPOOL
-using Microsoft.Extensions.ObjectPool;
 using System.Buffers;
 
 namespace MagicOnion.Internal;
 
 internal class StreamingHubPayloadPool
 {
-    const int MaximumRetained = 2 << 7;
-
-    readonly ObjectPool<StreamingHubPayloadCore> pool = new DefaultObjectPool<StreamingHubPayloadCore>(new Policy(), MaximumRetained);
-
     public static StreamingHubPayloadPool Shared { get; } = new();
 
     public StreamingHubPayload RentOrCreate(ReadOnlySequence<byte> data)
     {
-        var payload = pool.Get();
-        payload.Initialize(data);
 #if DEBUG
+        var payload = new StreamingHubPayloadCore();
+        payload.Initialize(data);
         return new StreamingHubPayload(payload);
 #else
-        return (StreamingHubPayload)payload;
+        var payload = new StreamingHubPayload();
+        payload.Initialize(data);
+        return payload;
 #endif
     }
 
     public StreamingHubPayload RentOrCreate(ReadOnlySpan<byte> data)
     {
-        var payload = pool.Get();
-        payload.Initialize(data);
 #if DEBUG
+        var payload = new StreamingHubPayloadCore();
+        payload.Initialize(data);
         return new StreamingHubPayload(payload);
 #else
-        return (StreamingHubPayload)payload;
+        var payload = new StreamingHubPayload();
+        payload.Initialize(data);
+        return payload;
 #endif
     }
 
     public StreamingHubPayload RentOrCreate(ReadOnlyMemory<byte> data, bool holdReference)
     {
-        var payload = pool.Get();
-        payload.Initialize(data, holdReference);
 #if DEBUG
+        var payload = new StreamingHubPayloadCore();
+        payload.Initialize(data, holdReference);
         return new StreamingHubPayload(payload);
 #else
-        return (StreamingHubPayload)payload;
+        var payload = new StreamingHubPayload();
+        payload.Initialize(data, holdReference);
+        return payload;
 #endif
     }
 
     public void Return(StreamingHubPayload payload)
     {
-#if DEBUG
-        payload.MarkAsReturned();
-        pool.Return(payload.Core);
-#else
-        pool.Return(payload);
-#endif
-    }
-
-    class Policy : IPooledObjectPolicy<StreamingHubPayloadCore>
-    {
-        public StreamingHubPayloadCore Create()
-        {
-#if DEBUG
-            return new StreamingHubPayloadCore();
-#else
-            return new StreamingHubPayload();
-#endif
-        }
-
-        public bool Return(StreamingHubPayloadCore obj)
-        {
-            obj.Uninitialize();
-            return true;
-        }
     }
 }
 #endif
