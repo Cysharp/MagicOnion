b4b9e768d9f540eb15088b008f177822063fd200
rhee <locus84@gmail.com>
2025-02-02 15:55:43 +0900

    fix : swagger needs AddEndpointsApiExplorer add: sample for jwt security scheme

--- samples/JsonTranscoding/JsonTranscodingSample.Server/Program.cs
+++ samples/JsonTranscoding/JsonTranscodingSample.Server/Program.cs
@@ -5,13 +5,24 @@ builder.Services.AddGrpc();
 
 // Add MagicOnion services to the container and enable JSON transcoding feature.
 builder.Services.AddMagicOnion().AddJsonTranscoding();
+
 // Add MagicOnion JSON transcoding Swagger support.
 builder.Services.AddMagicOnionJsonTranscodingSwagger();
+
+builder.Services.AddEndpointsApiExplorer();
+
+// // JwtBearer authentication
+// builder.Services.AddAuthentication();
+// builder.Services.AddAuthorization();
+
 // Add Swagger generator services.
 builder.Services.AddSwaggerGen(options =>
 {
     // Reflect the XML documentation comments of the service definition in Swagger.
     options.IncludeMagicOnionXmlComments(Path.Combine(AppContext.BaseDirectory, "JsonTranscodingSample.Shared.xml"));
+
+    // // Add JWT security scheme to Swagger. 
+    // options.AddJwtSecurityScheme();
 });
 
 var app = builder.Build();
